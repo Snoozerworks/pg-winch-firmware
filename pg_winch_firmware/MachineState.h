Snoozerworks/pg-winch-firmware

commit 6fcfd1a4569b2a98ce3c1ef5871028bae098b348

Changed file some names. Extracted machine state struct into MachineState.h and moved some functions along. Reformatting and refactoring.

pg_winch_firmware/MachineState.h
@@ -1,0 +1,172 @@
+/*
+ * machine_state.h
+ *
+ *  Created on: 16 aug 2014
+ *      Author: markus
+ */
+
+#ifndef MACHINESTATE_H
+#define MACHINESTATE_H
+
+// Project files to include
+#include "types.h"
+#include "parameter.h"
+
+/**
+ * Namespace for state variables and functions.
+ */
+class MachineState {
+public:
+	volatile byte _drum_ticks; 	// Tachometer counter used by interrupt 1
+	volatile byte _pump_ticks; 	// Tachometer counter used by interrupt 0
+
+	unsigned long mark_time;	// Time mark
+	unsigned long time;			// Timestamp
+	byte mode; 					// Mode of operation
+	byte errors; 				// Error states (active/inactive)
+	byte drum_err_cnt; 			// Drum speed error counter
+	byte pump_err_cnt; 			// Pump speed error counter
+
+	byte drum_spd;				// Sampled drum speed in 2 x pulse/sample
+	byte pump_spd;				// Sampled pump speed in 2 x pulse/sample
+	byte drum_spd_f;			// Filtered drum speed.
+	byte pump_spd_f;			// Filtered pump speed.
+
+	int temp; 					// Oil temperature
+	int pres; 					// Pump pressure
+
+	// Inputs
+	byte sw; 					// Switch states bit field (on/off)
+
+	/**
+	 * Returns true if at least one bit in bitfield is active in in bitmask.
+	 *
+	 * @param byte Bits to look for in bitmask.
+	 * @param byte Bits to compare bitfield with.
+	 * @return boolean True if any bit is set.
+	 */
+	static boolean chk_bits(byte& bitfield, byte bitmask) {
+		return (bitfield & bitmask);
+	}
+
+	/**
+	 * Sets or clears bits in bitfield to bits in bitmask. If set is false, bits
+	 * are unset otherwise they are set.
+	 *
+	 * @param set Flag true to set bits. Unset otherwise.
+	 * @param bitfield Byte to set or unset bits in.
+	 * @param bitmask Byte with bits to set or unset.
+	 */
+	static void set_bits(boolean set, byte& bitfield, byte bitmask) {
+		if (set) {
+			bitfield |= bitmask;
+		} else {
+			bitfield &= ~bitmask;
+		}
+	}
+
+	/**
+	 * Sample tachometer counts.
+	 *
+	 * Values of drum_spd, pump_spd and its filtered counterparts drum_spd_f
+	 * and pump_spd_f are updated.
+	 *
+	 * The low-pass filter uses a local constant FILTER_SHIFT sets the filter
+	 * characteristics.
+	 *
+	 * Also see http://www.edn.com/design/systems-design/4320010/A-simple-software-lowpass-filter-suits-embedded-system-applications *
+	 */
+	void read_tachometers() {
+		static unsigned int drum_filter = 0; // Filter
+		static unsigned int pump_filter = 0; // Filter
+
+		drum_spd = _drum_ticks;
+		pump_spd = _pump_ticks;
+
+		drum_spd <<= 1; // Note multiplication by 2!
+		pump_spd <<= 1; // Note multiplication by 2!
+
+		drum_filter = drum_filter - (drum_filter >> FILTER_SHIFT) + drum_spd;
+		drum_spd_f = (byte) (drum_filter >> FILTER_SHIFT);
+
+		pump_filter = pump_filter - (pump_filter >> FILTER_SHIFT) + pump_spd;
+		pump_spd_f = (byte) (pump_filter >> FILTER_SHIFT);
+
+	}
+
+	/**
+	 * Reads digital inputs, check serial for commands and updates machine state
+	 * accordingly. Does not read tachometers.
+	 */
+	void read_switches() {
+		byte rx_cmd; // Serial command byte
+
+		// Read from serial to get any pending commands.
+		rx_cmd = (Serial.available()) ? (byte) Serial.read() : C::CM_NOCMD;
+		if (rx_cmd > C::_CM_LAST) {
+			rx_cmd = C::CM_NOCMD;
+		}
+
+		// Read switches and update machine switch states.
+		set_bits(Pins::SW_NE.read() == LOW, sw, C::SW_NE);
+		set_bits(Pins::SW_SE.read() == LOW || rx_cmd == C::CM_SE, sw, C::SW_SE);
+		set_bits(Pins::SW_UP.read() == LOW || rx_cmd == C::CM_UP, sw, C::SW_UP);
+		set_bits(Pins::SW_DN.read() == LOW || rx_cmd == C::CM_DN, sw, C::SW_DN);
+		set_bits(rx_cmd == C::CM_SP, sw, C::SW_SP);
+		set_bits(rx_cmd == C::CM_GT, sw, C::SW_GT);
+
+		// Read pressure
+		pres = Pins::PRESSURE.read();
+
+		// Check I2C bus error
+		set_bits(0 != I2c.read(I2C_TMP_ADDR, (uint8_t) 2), errors, C::ERR_TWI);
+
+		// Read temperature
+		temp = ((int) I2c.receive() << 8 | (int) I2c.receive()) >> 7;
+
+		// Check temperature fault conditions
+		set_bits(temp > P::params[P::I_OIL_HI].val, errors, C::ERR_TEMP_HIGH);
+		set_bits(temp < P::params[P::I_OIL_LO].val, errors, C::ERR_TEMP_LOW);
+	}
+
+	/**
+	 * Write sensor data to Serial.
+	 *
+	 * High byte are sent before low bytes (big endian byte order). In total 11
+	 * bytes are sent;
+	 *
+	 * byte  0 : mode
+	 * byte  1 : timestamp byte 1 (high byte)
+	 * byte  2 : timestamp byte 2
+	 * byte  3 : timestamp byte 3
+	 * byte  4 : timestamp byte 4 (low byte)
+	 * byte  5 : pump speed
+	 * byte  6 : drum speed
+	 * byte  7 : temperature (high byte)
+	 * byte  8 : temperature (low byte)
+	 * byte  9 : pressure (high byte)
+	 * byte 10 : pressure (low byte)
+	 *
+	 */
+	void serial_send() {
+		byte tx_buffer[11];
+		tx_buffer[0] = mode;
+		tx_buffer[1] = time >> 24;
+		tx_buffer[2] = time >> 16;
+		tx_buffer[3] = time >> 8;
+		tx_buffer[4] = time;
+		tx_buffer[5] = pump_spd;
+		tx_buffer[6] = drum_spd;
+		tx_buffer[7] = temp >> 8;
+		tx_buffer[8] = temp;
+		tx_buffer[9] = pres >> 8;
+		tx_buffer[10] = pres;
+
+		Serial.write(tx_buffer, sizeof(tx_buffer));
+		Serial.flush();
+	}
+
+};
+// End namespace MachineState
+
+#endif /* MACHINESTATE_H */

pg_winch_firmware/Controller.h
@@ -1,0 +1,111 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "Arduino.h"
+#include "parameter.h"
+
+/**
+ * Controller "bang-bang PID" 
+ *
+ * Output calculated as Kp*error + Ki*integral + Kd*derivative.
+ * Integral can be limited by imax. Output is limited to 0..255.
+ *  
+ * Note!
+ * Kp, Ki and Kd are specified by global parameters I_PID_P, I_PID_I and
+ * I_PID_D. These must not be outside range -128..127 !
+ * 
+ * Controller process value PV range is 0..255.
+ * If controller error (setpoint-PV) exceed +/-127 controller output is
+ * maximized (minimized) to 0 or 255.
+ */
+class Controller {
+private:
+	int error;
+	int integral;
+	int preError;
+	int derivative;
+
+public:
+	byte setpoint; // Controller setpoint value (pump speed)
+
+	/**
+	 * Reset controller to zero (p,i and d parts) except setpoint value.
+	 *
+	 * @returns void
+	 */
+	void reset() {
+		// Private initiation
+		error = 0;
+		preError = 0;
+		integral = 0;
+		derivative = 0;
+	}
+
+	/**
+	 * The controller to regulate pump speed.
+	 *
+	 * The drum speed is needed to linearise the output. Compensation for
+	 * throttle response at different engine speeds is made. The engine speed
+	 * (or rather gearbox out speed) is proportional to the sum of drum and pump
+	 * speed which is used for the compensation.
+	 *
+	 * See parameters.h for parameters to adjust the controller.
+	 *
+	 * @param byte pump_spd Process value 0-255.
+	 * @param byte drum_spd Process value 0-255.
+	 * @return byte signal 0-255.
+	 */
+	byte process(byte pump_spd, byte drum_spd) {
+		using namespace P;
+
+		int eng_spd_comp; // Gain compensation for engine speed
+		long output;
+
+		// calculate error
+		error = setpoint - pump_spd; // [-127,127] (limited below)
+
+		// Maximise/minimise output if error is too big/small.
+		if (error > 127) {
+			integral = derivative = 0;
+			return 255;
+		}
+		if (error < -127) {
+			integral = derivative = 0;
+			return 0;
+		}
+
+		// track error over time
+		integral = constrain(integral + error, 0, params[I_PID_IMAX].val); // [0, 255]
+
+		// determine the amount of change from the last time checked
+		derivative = constrain(error - preError, -64, 64); // [-64770, 64770] => [-64, 64]
+
+		// remember the error for the next time around.
+		preError = error;
+
+		// To linearise throttle response, which is believed to be a function of
+		// engine speed, the compensation factor eng_spd_comp is used. Regulator
+		// output is then basically calculated as
+		//    "PID value / (engine speed/k + k)" where k is params[I_PID_K]
+		// I.e. the higher engine speed the lower the gain. Note that changing k
+		// may also require adjustment of p, i and d gains.
+		// (Well, engine speed is in fact gearbox out speed as we don't know
+		// what gear is actually engaged.)
+
+		eng_spd_comp = ((int) pump_spd * PPT_DRUM + (int) drum_spd * PPT_PUMP)
+				>> 6; // [0, 71]
+		eng_spd_comp = 256 + eng_spd_comp * params[I_PID_K].val; // [-2016, 2528] , -32, 32
+		eng_spd_comp = max(0, eng_spd_comp); // [0, 2528]
+
+		output = params[I_PID_P].val * error + // [-4064, 4064]
+				params[I_PID_I].val * integral + // [0, 8160]
+				params[I_PID_D].val * derivative; // [-2048, 2048]  ==> tot [-6112, 14272]
+		output = (output * (long) eng_spd_comp) >> 11; // [-15451136, 36079616], [-7544, 17617]
+		output = constrain(output, 0, 255);
+
+		return (byte) output;
+	}
+
+};
+
+#endif /* CONTROLLER_H */

pg_winch_firmware/ThrottleServo.h
@@ -1,0 +1,43 @@
+#ifndef THROTTLESERVO_H
+#define THROTTLESERVO_H
+
+#include "Arduino.h"
+#include "parameter.h"
+
+//class ThrottleServo: public Servo {
+class ThrottleServo: public Servo {
+public:
+	/**
+	 * Servo position 0-255. A value of 0 means the short servo pulse is used and
+	 * a value of 255 means the long servo pulse is used. Pulse lengths are set by
+	 * parameters params[I_SERV_LO].val and params[I_SERV_HI].val.
+	 */
+	byte pos;
+
+	/**
+	 * Set servo to 0 position.
+	 */
+	void reset() {
+		using namespace P;
+		pos = 0;
+		set();
+		delay(params[I_SERV_RST].val);
+	}
+
+	/**
+	 * Set position of servo 0-255
+	 */
+	void set() {
+		using namespace P;
+		int us;
+		us =
+				params[I_SERV_LO].val
+						+ (int) pos
+								* ((params[I_SERV_HI].val
+										- params[I_SERV_LO].val) / 255);
+		writeMicroseconds(us);
+	}
+
+};
+
+#endif /* THROTTLESERVO_H */

pg_winch_firmware/controller.h
@@ -1,111 +1,0 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
-
-#include "Arduino.h"
-#include "parameter.h"
-
-/**
- * Controller "bang-bang PID" 
- *
- * Output calculated as Kp*error + Ki*integral + Kd*derivative.
- * Integral can be limited by imax. Output is limited to 0..255.
- *  
- * Note!
- * Kp, Ki and Kd are specified by global parameters I_PID_P, I_PID_I and
- * I_PID_D. These must not be outside range -128..127 !
- * 
- * Controller process value PV range is 0..255.
- * If controller error (setpoint-PV) exceed +/-127 controller output is
- * maximized (minimized) to 0 or 255.
- */
-class Controller {
-private:
-  int error;
-  int integral;
-  int preError;
-  int derivative;
-
-public:
-  byte setpoint; // Controller setpoint value (pump speed)
-
-  /**
-   * Reset controller to zero (p,i and d parts) except setpoint value.
-   *
-   * @returns void 
-   */
-  void reset() {
-    // Private initiation
-    error = 0;
-    preError = 0;
-    integral = 0;
-    derivative = 0;
-  }
-
-  /**
-   * The controller to regulate pump speed.
-   *
-   * The drum speed is needed to linearize the output. Compensation for throttle 
-   * repsonse at different engine speeds is made. The engine speed (or rather 
-   * gearbox out speed) is propotional to the sum of drum and pump 
-   * speed which is used for the compensation.
-   * 
-   * See parameters.h for parameters to adjust the controller.
-   * 
-   * @param byte pump_spd Process value 0-255.
-   * @param byte drum_spd Process value 0-255.
-   * @return byte signal 0-255.
-   */
-  byte process(byte pump_spd, byte drum_spd) {
-    using namespace P;
-
-    int eng_spd_comp; // Gain compensation for engine speed
-    long output;
-
-    // calculate error
-    error = setpoint - pump_spd; // [-127,127] (limited below)
-
-    // Maximize/minimize output if error is too big/small.
-    if (error > 127) {
-      integral = derivative = 0;
-      return 255;
-    }
-    if (error < -127) {
-      integral = derivative = 0;
-      return 0;
-    }
-
-    // track error over time
-    integral = constrain(integral + error, 0, params[I_PID_IMAX].val); // [0, 255]
-
-    // determine the amount of change from the last time checked
-    derivative = constrain(error - preError, -64, 64); // [-64770, 64770] => [-64, 64]
-
-    // remember the error for the next time around.
-    preError = error;
-
-    // To linearize throttle response, which is belived to be a function of 
-    // engine speed, the compensation factor eng_spd_comp is used. Regulator 
-    // output is then basically calculated as 
-    //    "PID value / (engine speed/k + k)" where k is params[I_PID_K]
-    // I.e. the higher engine speed the lower the gain. Note that changing k
-    // may also require adjustment of p, i and d gains.
-    // (Well, engine speed is in fact gearbox out speed as we don't know what 
-    // gear is actually engaged.)
-
-    eng_spd_comp = ((int) pump_spd * PPT_DRUM + (int) drum_spd * PPT_PUMP) >> 6; // [0, 71]
-    eng_spd_comp = 256 + eng_spd_comp * params[I_PID_K].val; // [-2016, 2528] , -32, 32
-    eng_spd_comp = max(0, eng_spd_comp); // [0, 2528]
-
-    output = params[I_PID_P].val * error + // [-4064, 4064]
-            params[I_PID_I].val * integral + // [0, 8160]
-            params[I_PID_D].val * derivative; // [-2048, 2048]  ==> tot [-6112, 14272]            
-    output = (output * (long) eng_spd_comp) >> 11; // [-15451136, 36079616], [-7544, 17617]
-    output = constrain(output, 0, 255);
-
-
-    return (byte) output;
-  }
-
-};
-
-#endif

pg_winch_firmware/parameter.h
@@ -41,7 +41,7 @@ const byte TACH_DRUM_ERR_COUNT = 2; // Number of samples with drum zero speed be
 const byte TACH_DRUM_ERR_SERVO_TRESHOLD = 95; // Max servo setpoint value allowed before resetting servo due to drum zero speed.
 
 // Tachometer filter parameter see types.h
-//const byte FILTER_SHIFT = 2; // Shift parameter. 1-3 should likely suffice.
+const byte FILTER_SHIFT = 1; // Shift parameter. 1-3 should likely suffice.
 
 // Manual throttle step (used in idle mode)
 const byte MAN_THROTTLE_STEP = 10;
@@ -122,23 +122,21 @@ const byte OPER_PARAM_END = I_OIL_LO;
 const byte INST_PARAM_END = I_PID_IMAX;
 
 // See Parameter type definition for how to define a parameter.
-Parameter params[] = {
-// {0, "Max linhastighet", 36, 20, 48, 1, 20 / T_SAMPLE / PPT_DRUM * 2262, 48 / T_SAMPLE / PPT_DRUM * 2262},
-// {1, "Pumpvarv", 16, 6, 36, 1, 6 / T_SAMPLE / PPT_PUMP * 30000, 36 / T_SAMPLE / PPT_PUMP * 30000},
+Parameter params[] = { //
 		{ 0, "Max linhastighet", 50, 20, 70, 1, 28, 100 }, //
-		{ 1, "Pumpvarv", 13, 6, 36, 1, 90, 540 }, //
-		{ 2, "Max oljetemp.", 140, 100, 180, 2, 50, 90 }, //
-		{ 3, "Min oljetemp.", 20, 20, 80, 2, 10, 40 }, //
-		{ 4, "Servo min-puls", 800, 600, 2400, 20, 600, 2400 }, //
-		{ 5, "Servo max-puls", 2140, 600, 2400, 20, 600, 2400 }, //
-		{ 6, "Servo reset tid", 250, 100, 600, 25, 100, 600 }, //
-		{ 7, "PID p", 13, 1, 32, 1, 1, 32 }, //
-		{ 8, "PID i", 5, 0, 32, 1, 0, 32 }, //
-		{ 9, "PID d", 7, 0, 32, 1, 0, 32 }, //
-		{ 10, "PID k", -17, -32, 32, 1, -32, 32 }, //
-		{ 11, "PID i gr\xE1ns", 95, 0, 255, 1, 0, 255 } //
-};
+				{ 1, "Pumpvarv", 13, 6, 36, 1, 90, 540 }, //
+				{ 2, "Max oljetemp.", 140, 100, 180, 2, 50, 90 }, //
+				{ 3, "Min oljetemp.", 20, 20, 80, 2, 10, 40 }, //
+				{ 4, "Servo min-puls", 800, 600, 2400, 20, 600, 2400 }, //
+				{ 5, "Servo max-puls", 2140, 600, 2400, 20, 600, 2400 }, //
+				{ 6, "Servo reset tid", 250, 100, 600, 25, 100, 600 }, //
+				{ 7, "PID p", 13, 1, 32, 1, 1, 32 }, //
+				{ 8, "PID i", 5, 0, 32, 1, 0, 32 }, //
+				{ 9, "PID d", 7, 0, 32, 1, 0, 32 }, //
+				{ 10, "PID k", -17, -32, 32, 1, -32, 32 }, //
+				{ 11, "PID i gr\xE1ns", 95, 0, 255, 1, 0, 255 } //
+		};
 
 }
 
-#endif
+#endif /* PARAMETERS_H */

commit 98800bee0f3bb8883f638b96ed809ef2c2583ef4

Fixed drum and pump speed bug introduced in commit 6fcfd1a4569b2a98ce3c1ef5871028bae098b348.

pg_winch_firmware/MachineState.h
@@ -77,18 +77,30 @@ public:
 	 * Also see http://www.edn.com/design/systems-design/4320010/A-simple-software-lowpass-filter-suits-embedded-system-applications *
 	 */
 	void read_tachometers() {
-		static unsigned int drum_filter = 0; // Filter
-		static unsigned int pump_filter = 0; // Filter
+	static byte drum_ticks_old = 0;				// Previous sample of drum tachometer count
+		static byte pump_ticks_old = 0;			// Previous sample of pump tachometer count
+		byte drum_ticks;						// Drum tachometer count
+		byte pump_ticks;						// Pump tachometer count
+		static unsigned int drum_filter = 0; 	// Filter
+		static unsigned int pump_filter = 0; 	// Filter
 
-		drum_spd = _drum_ticks;
-		pump_spd = _pump_ticks;
+		// Read volatile variables just once
+		drum_ticks = _drum_ticks;
+		pump_ticks = _pump_ticks;
 
-		drum_spd <<= 1; // Note multiplication by 2!
-		pump_spd <<= 1; // Note multiplication by 2!
+		// Update speed. Note multiplication by 2!
+		drum_spd = (drum_ticks - drum_ticks_old) << 1;
+		pump_spd = (pump_ticks - pump_ticks_old) << 1;
 
+		// Update tachometer count history
+		drum_ticks_old = drum_ticks;
+		pump_ticks_old = pump_ticks;
+
+		// Filter drum speed
 		drum_filter = drum_filter - (drum_filter >> FILTER_SHIFT) + drum_spd;
 		drum_spd_f = (byte) (drum_filter >> FILTER_SHIFT);
 
+		// Filter pump speed
 		pump_filter = pump_filter - (pump_filter >> FILTER_SHIFT) + pump_spd;
 		pump_spd_f = (byte) (pump_filter >> FILTER_SHIFT);
 